8a942069265b305fc68539503e3c495928c861f9
github username <github email address>
2018-03-11 16:34:59 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = ["Rice", "Potatoes", "Pasta"]
 
   def call(env)
     resp = Rack::Response.new
@@ -10,6 +11,23 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      # search_term = req.params["q"]
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |cart|
+          resp.write "#{cart}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+        item_to_add = req.params["item"]
+      if @@items.include? item_to_add
+        @@cart << item_to_add
+          resp.write "added #{item_to_add}"
+      else
+        resp.write "We don't have that item!"
+  end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
